83476dc889811e0860c6446e754740ea4af4a17d
litsame <61036658+litsame@users.noreply.github.com>
2021-12-08 03:42:26 +0000

    Created project Servidor & GRDS. Added UDP connection between Cliente/Servidor and GRDS

---
+++ Meta2/GRDS/src/GRDS.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.net.*;
+
+public class GRDS {
+
+    private static final int MAX_SIZE = 1024;
+    private static final String SERVER_REQUEST = "SERVER_GET_ADDR_PORT_TCP";
+    private static final String CLIENT_REQUEST = "GET_ADDR_PORT_TCP";
+    private int serverCount = 0;
+
+    public static void main(String[] args) {
+        InetAddress AddrGRDS;
+        int listeningPort;
+        DatagramSocket socket = null;
+        DatagramPacket packet;
+        String response;
+
+        if(args.length != 1){
+            System.out.println("Sintaxe: java GRDS listeningPort");
+            return;
+        }
+
+        try{
+
+            listeningPort = Integer.parseInt(args[0]);
+            System.out.println("Listening on port " + listeningPort);
+
+            socket = new DatagramSocket(listeningPort);
+            while(true) {
+                packet = new DatagramPacket(new byte[MAX_SIZE], MAX_SIZE);
+                socket.receive(packet);
+
+                response = new String(packet.getData(), 0, packet.getLength());
+
+                if (response.equals(SERVER_REQUEST)) {
+                    System.out.println("Sent message to server.");
+                    packet.setData("msg received server".getBytes(), 0, "msg received server".length());
+                }
+                else if (response.equals(CLIENT_REQUEST)){
+                    System.out.println("Sent message to client.");
+                    packet.setData("msg received client".getBytes(), 0, "msg received client".length());
+                }
+
+                InetAddress clientAddress = packet.getAddress();
+                int clientPort = packet.getPort();
+
+
+                socket.send(packet);
+
+            }
+        }catch(UnknownHostException e){
+            System.out.println("Destino desconhecido:\n\t"+e);
+        }catch(NumberFormatException e){
+            System.out.println("O porto do servidor deve ser um inteiro positivo.");
+        }catch(SocketTimeoutException e){
+            System.out.println("Nao foi recebida qualquer resposta:\n\t"+e);
+        }catch(SocketException e){
+            System.out.println("Ocorreu um erro ao nivel do socket UDP:\n\t"+e);
+        }catch(IOException e){
+            System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
+        }finally{
+            if(socket != null){
+                socket.close();
+            }
+        }
+    }
+}
--- Meta2/Servidor/src/Servidor.java
+++ Meta2/Servidor/src/Servidor.java
@@ -1,14 +1,12 @@
 import java.io.IOException;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Main {
+public class Servidor {
+
     public static final int MAX_SIZE = 1024;
-    public static final String ADDR_PORT_REQUEST = "GET_ADDR_PORT_TCP";
+    public static final String ADDR_PORT_REQUEST = "SERVER_GET_ADDR_PORT_TCP";
 
-    public static void main(String args[]) {
-
+    public static void main(String[] args) {
         InetAddress AddrGRDS;
         int PortGRDS;
         DatagramSocket SocketGRDS = null;
@@ -16,7 +14,7 @@ public class Main {
         String responseGRDS;
 
         if (args.length != 2){
-            System.out.println("Sintaxe: java Cliente serverAddress serverUdpPort");
+            System.out.println("Sintaxe: java Servidor serverAddress serverUdpPort");
             return;
         }
 
@@ -53,46 +51,5 @@ public class Main {
                 SocketGRDS.close();
             }
         }
-
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> credentials = new ArrayList<>(2);
-        int option = 0;
-        User user = new User();
-
-        if (!user.isSession()) {
-            System.out.println("1 - Iniciar sessão");
-            System.out.println("2 - Criar conta");
-        }
-        System.out.println("3 - Fechar cliente");
-        System.out.println();
-        System.out.print("Opção: ");
-        while (!sc.hasNextInt());
-            option = sc.nextInt();
-
-        if (option == 1 && !user.isSession()) {
-            getUserCredentials(credentials);
-            user.login(credentials.get(0), credentials.get(1));
-        }
-        else if (option == 2 && !user.isSession()) {
-            getUserCredentials(credentials);
-            user = new User(credentials.get(0), credentials.get(1));
-            user.createAccount();
-        }
-        else if (option == 3)
-            return;
-        else {
-            System.out.println("Opção inexistente.");
-        }
-
-    }
-
-    public static void getUserCredentials(ArrayList<String> cred) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Username: ");
-        cred.add(sc.nextLine());
-
-        System.out.print("Password: ");
-        cred.add(sc.nextLine());
     }
 }
